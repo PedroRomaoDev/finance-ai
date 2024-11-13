aa1365258ab27d151d98ee02a375e2dd22bbcdc1
PedroRomaoDev <pedro7ntj@gmail.com>
2024-11-13 17:56:10 -0300

    feat: create add transaction dialog

---
+++ app/_components/money-input.tsx
@@ -0,0 +1,24 @@
+import { Input, InputProps } from "@/app/_components/ui/input";
+import { NumericFormat, NumericFormatProps } from "react-number-format";
+import { forwardRef } from "react";
+
+export const MoneyInput = forwardRef(
+  (
+    props: NumericFormatProps<InputProps>,
+    ref: React.ForwardedRef<HTMLInputElement>,
+  ) => {
+    return (
+      <NumericFormat
+        {...props}
+        thousandSeparator="."
+        decimalSeparator=","
+        prefix="R$ "
+        allowNegative={false}
+        customInput={Input}
+        getInputRef={ref}
+      />
+    );
+  },
+);
+
+MoneyInput.displayName = "MoneyInput";
